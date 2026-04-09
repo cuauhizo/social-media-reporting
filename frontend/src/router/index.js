2bef7cbd80f08872700fdfa95a9a738761976455
cuauhizo <cuauhizo@gmail.com>
2026-04-09 12:45:12 -0600

    titulo en pestañas

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -9,13 +9,23 @@ const router = createRouter({
       path: '/',
       name: 'report',
       component: ReportView,
+      meta: { title: 'Reporte Pluxee' },
     },
     {
       path: '/admin',
       name: 'admin',
       component: AdminView,
+      meta: { title: 'Admin | Subida de Datos' },
     },
   ],
+})
+
+router.beforeEach((to, from, next) => {
+  // Busca el título en los metas de la ruta a la que vamos, si no hay, usa uno por defecto
+  document.title = to.meta.title || 'Social Reporting'
+
+  // Le decimos al router que continúe con la navegación normal
+  next()
 })
 
 export default router
