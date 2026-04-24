3c3024767ee10d2ec35c1e1fd658adfc5abae3d3
cuauhizo <cuauhizo@gmail.com>
2026-04-24 17:02:39 -0600

    imagenes

--- frontend/src/components/ContextSection.vue
+++ frontend/src/components/ContextSection.vue
@@ -1,12 +1,8 @@
 <template>
   <section class="pdf-page flex flex-col justify-center min-h-screen p-8 bg-white border-b border-gray-200">
     <div class="max-w-5xl mx-auto flex flex-col md:flex-row gap-12 items-center w-full no-break">
-      <div class="w-40 h-40 bg-pluxeeYellow shrink-0 flex justify-center items-center shadow-lg clip-x">
-        <div class="w-32 h-32 bg-white transform rotate-45 flex justify-center items-center">
-          <div class="w-24 h-24 bg-pluxeeYellow"></div>
-        </div>
-      </div>
-
+      <!-- <img src="../../public/pluxee.svg" class="w-40" alt="pluxee" /> -->
+      <img src="../../public/x.svg" class="w-80" alt="pluxee" />
       <div class="flex-1">
         <h2 class="text-4xl font-black text-pluxeeBlue mb-6 uppercase tracking-wide keep-with-next">
           {{ data.title || 'Contexto Actual (RRSS)' }}
