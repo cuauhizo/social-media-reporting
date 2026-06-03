9390e9030c4058cb22b7a03be3ed58a6f992bf5a
cuauhizo <cuauhizo@gmail.com>
2026-06-03 12:09:25 -0600

    fecha front

--- frontend/src/views/ReportView.vue
+++ frontend/src/views/ReportView.vue
@@ -103,7 +103,7 @@
 
 <template>
   <div class="font-sans text-gray-800 bg-gray-50">
-    <div class="bg-pluxeeBlue p-4 hidden justify-center items-center gap-4 text-white print:hidden">
+    <div class="bg-pluxeeBlue p-4 justify-center items-center gap-4 text-white print:hidden">
       <span class="text-xs font-bold uppercase tracking-widest opacity-70">Viendo reporte de:</span>
       <input type="month" v-model="selectedPeriod" class="bg-white/10 border border-white/20 rounded-lg px-3 py-1 font-black outline-none cursor-pointer hover:bg-white/20 transition" />
     </div>
